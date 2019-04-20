ad03d678fe0e381f073ae65ff9a2fb15d4cc5698
Shashank Gugnani <gugnani.2@osu.edu>
2019-04-20 16:37:11 -0400

    add missing .sh extension

--- scripts/sc19_experiments.sh
+++ scripts/sc19_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ### Scripts to run/reproduce SC'19 experiments
-### Usage: ./sc19_experiments
+### Usage: ./sc19_experiments.sh
 
-./submit_jobs 1 8
-./submit_jobs 2 8
-./submit_jobs 4 8
-./submit_jobs 8 8
-./submit_jobs 16 8
+./submit_jobs.sh 1 8
+./submit_jobs.sh 2 8
+./submit_jobs.sh 4 8
+./submit_jobs.sh 8 8
+./submit_jobs.sh 16 8
